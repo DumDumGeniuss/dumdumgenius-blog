13bb08672b6bc5b7dea7aa0b418972a4273bb9f5
dumdumgeniuss <dumdumgenius@gmail.com>
2016-05-24 21:34:46 +0800

    Finish React environment

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import  ReactDOM from 'react-dom';
+import IndexScss from '../scss/index.scss';
+import Layout from './layout/Layout.js'
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout />, app);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + '/src',
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: __dirname + "/src/js/index.js",
+    output: {
+        path: __dirname + "/src",
+        filename: "main.min.js"
+    },
+    module: {
+        loaders: [
+            { 
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                test:/\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+
+        ],
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ],
+};

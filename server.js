02e31efd8b5fba5068bc4369844994ef5827ee35
dumdumgeniuss <dumdumgenius@gmail.com>
2016-07-07 23:38:56 +0800

    Add initial state and initial og tag by server side rendering

--- server.js
+++ server.js
@@ -12,19 +12,78 @@ import rootReducer from './dist/reducers'
 import AboutMe from './dist/pages/aboutMe/AboutMe'
 import routes from './dist/route/routes'
 import IndexLayout from './dist/IndexLayout'
+import firebase from './dist/services/firebase'
+
+import config from './server/config/production'
+
 
 const app = Express()
-const port = 4000
+const port = config.port
 app.use(Express.static(__dirname + '/public'))
-app.use(handleRender)
 
-function handleRender(req, res) {
-    console.log(req.url)
-  	// Create a new Redux store instance
-    let initialState = {}
+app.use('/diaries/:id', function(req, res) {
+    const query = req.query,
+        params = req.params,
+        completeUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+
+    firebase.database().ref('diaries/' + query.category + '/datas/' + params.id).once('value')
+        .then(function(result) {
+            const diary = result.val(),
+                initialState = {
+                    diaries: {
+                        diary: diary
+                    }
+                },
+                ogTagParams = {
+                    url: completeUrl,
+                    type: 'diary',
+                    title: diary.title,
+                    description: diary.content
+                }
+            handleRender(req, res, initialState, ogTagParams)
+        })
+})
+
+app.use('/masterpieces', function(req, res) {
+    const query = req.query,
+        params = req.params,
+        completeUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+    firebase.database().ref('paintings').once('value')
+        .then(function(result) {
+            const paintings = result.val(),
+                initialState = {
+                    paintings: {
+                        paintings: paintings
+                    }
+                },
+                ogTagParams = {
+                    url: completeUrl,
+                    type: 'paintings',
+                    title: "dumdumgenius' paintings",
+                    description: "I like paintings, althought I'm not not so well on it."
+                        + " But, you cant fall in love with something by no reason, right ?"
+                }
+            handleRender(req, res, initialState, ogTagParams)
+        })
+})
+
+app.use('*', function(req, res) {
+    const initialState = {},
+        ogTagParams = {
+            url: "http://dumdumgenius.com",
+            type: 'blog',
+            title: "DumDumGenius' Blog",
+            description: "Welcome to dumdumgenius' blog, it's my first blog, enjoy every thing you see"
+                + " and of course, if you have any advice to me, please contact me, the email can be found"
+                + " in about me page, thank you, sincerely, Messi Yang"
+        }
+    handleRender(req, res, initialState, ogTagParams)
+})
+
+function handleRender(req, res, initialState, ogTagParams) {
 
   	const store = createStore(rootReducer, initialState),
-  	    location = createLocation(req.url)
+  	    location = createLocation(req.originalUrl)
 
   	match({ routes, location }, function(error, redirectLocation, renderProps) {
     	// if (err) { 
@@ -45,24 +104,24 @@ function handleRender(req, res) {
   		const initialState = store.getState()
 
 	
-  		res.send(renderFullPage(html, initialState))
+  		res.send(renderFullPage(html, initialState, ogTagParams))
 	})
 }
 
-function renderFullPage(html, initialState) {
+function renderFullPage(html, initialState, ogTagParams) {
   	return `
   	  	<!doctype html>
   	  	<html>
   	  	  	<head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta property="og:url" content="http://dumdumgenius.com" />
-                <meta property="og:type" content="blog" />
-                <meta property="og:title" content="DumDumGenius' Blog" />
-                <meta property="og:description" content="This is DumDumGenius' (Messi Yang) blog, which contains my profile , masterpieces, also my tutorials and diaries." />
+                <meta property="og:url" content="${ogTagParams.url}" />
+                <meta property="og:type" content="${ogTagParams.type}" />
+                <meta property="og:title" content="${ogTagParams.title}" />
+                <meta property="og:description" content="${ogTagParams.description}" />
                 <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/myblog-1decf.appspot.com/o/selfPhoto2.jpg?alt=media" />
                 <meta property="og:image:width" content="200" />
                 <meta property="og:image:height" content="250" />
-  	  	  	 	  <title>DumDumGenius' blog</title>
+  	  	  	 	<title>${ogTagParams.title}</title>
                 <style>
                     body {
                         margin: 0px;

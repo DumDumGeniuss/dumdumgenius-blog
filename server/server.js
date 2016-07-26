9ce9b2673f18197e6d232551af8cd1e8bb5d605a
DumDumGeniuss <dumdumgenius@gmail.com>
2016-07-26 19:39:37 +0800

    fix fb cover photo error

--- server/server.js
+++ server/server.js
@@ -153,9 +153,9 @@ function renderFullPage(html, initialState, ogTagParams) {
                 <meta property="og:type" content="${ogTagParams.type}" />
                 <meta property="og:title" content="${ogTagParams.title}" />
                 <meta property="og:description" content="${ogTagParams.description}" />
-                <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/myblog-1decf.appspot.com/o/coverImages/fbCoverPhoto.jpg.jpg?alt=media" />
-                <meta property="og:image:width" content="200" />
-                <meta property="og:image:height" content="250" />
+                <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/myblog-1decf.appspot.com/o/coverImages%2FfbCoverPhoto.jpg.jpg?alt=media" />
+                <meta property="og:image:width" content="400" />
+                <meta property="og:image:height" content="200" />
   	  	  	 	<title>${ogTagParams.title}</title>
                 <link rel="stylesheet" type="text/css" href="/styles.css">
                 <style>
